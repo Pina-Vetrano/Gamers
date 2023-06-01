465a5e7570abdd2fb5a0b1b7b6d244f69a41bd8a
Pina Vetrano <94454263+Pina-Vetrano@users.noreply.github.com>
2023-06-01 23:35:20 +0200

    font no found, no img aside, content more clean1/8

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -16,7 +16,7 @@ function toggleNavigation() {
  }
 }
 
-// changer le theme et le nom du bouton
+/* changer le theme et le nom du bouton
 
 let changeTheme = document.querySelector(".text__darklight");
 
@@ -50,7 +50,7 @@ function light(){
     document.body.setAttribute("data-theme", "light");
     changeTheme.innerHTML = "light-mode";
 }
-
+*/
 // date copyright
 let answerDate = document.querySelector(".date");
 let today = new Date();
